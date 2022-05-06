2bb8bb3bd9745ebc41bd6517c0ac18b31d9de967
김기범 <drogba@douzone.com>
2022-05-06 18:09:42 +0900

    config add

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Logger, Param, ParseIntPipe, Patch, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
 import { BoardStatus } from './board-status.enum';
 import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
@@ -9,17 +11,25 @@ import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 @Controller('boards')
 @UseGuards(AuthGuard())
 export class BoardsController {
+    private logger = new Logger('BoardController');
+
     constructor(private boardsService: BoardsService) {};
     
     @Get()
-    getAllBoards(): Promise<Board[]> {
-        return this.boardsService.getAllBoards();
+    getAllBoards(
+        @GetUser() user: User
+    ): Promise<Board[]> {
+        this.logger.verbose(`User ${user.username} trying to get all boards`);
+        return this.boardsService.getAllBoards(user);
     }
 
     @Post()
     @UsePipes(ValidationPipe)
-    createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
-        return this.boardsService.createBoard(createBoardDto);
+    createBoard(@Body() createBoardDto: CreateBoardDto,
+    @GetUser() user: User): Promise<Board> {
+        this.logger.verbose(`User ${user.username} createing a new board.
+        Payload: ${JSON.stringify(createBoardDto)} `);
+        return this.boardsService.createBoard(createBoardDto, user);
     }
 
     @Get('/:id')
@@ -28,8 +38,10 @@ export class BoardsController {
     }
 
     @Delete('/:id')
-    deleteBoard(@Param('id', ParseIntPipe) id: number): Promise<void> {
-        return this.boardsService.deleteBoard(id);
+    deleteBoard(
+        @Param('id', ParseIntPipe) id: number,
+        @GetUser() user: User): Promise<void> {
+        return this.boardsService.deleteBoard(id, user);
     }
 
     @Patch('/:id/status')
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const serverConfig = config.get('server');
   await app.listen(3000);
+  Logger.log(`Application running on port ${serverConfig.port}`);
 }
 bootstrap();
